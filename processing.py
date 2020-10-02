7f8e6cc128f6781a149d34928190c19cebe29ad9
IlyaVatnik <ilya.vatnik@gmail.com>
2020-10-02 18:31:07 +0700

    v1

--- processing.py
+++ processing.py
@@ -18,27 +18,47 @@ import heterodyning.agilent_bin_beta as b_reader
 
 formatter1 = EngFormatter(places=2)
 
-class Data():
+
+class Mode():
+    def __init__(self,ind,freq):
+        self.ind=ind
+        self.freq=freq
+        
+        self.birth_time=None
+        self.death_time=None
+        
+        self.life_time=None
+
+class Spectrogram():
     def __init__(self,f_name,
                  win_time=3e-6,
                  overlap_time=2.5e-6,
+                 IsAveraging=True,
                  average_time_window=50e-6,    
                  average_freq_window=10e6):
-        self.dict=b_reader.read(f_name)
+           
         self.win_time=win_time
         self.overlap_time=overlap_time
+        self.IsAveraging=IsAveraging
         self.average_time_window=average_time_window
         self.average_freq_window=average_freq_window
         
+        
+        self.dt=None
         self.freq=None
         self.time=None
         self.processed_spec=None
-        self.dt=self.dict['Channel_1']['x_increment']
         self.need_to_update_spec=True
         
         self.fig_spec=None
         self.ax_spec=None
-        self.mode_indexes=None
+        
+        self.modes=None
+        
+               
+        self.process_spectrogram(f_name)
+        
+        
         
         
     def set_params(self,win_time,overlap_time,
@@ -51,10 +71,10 @@ class Data():
         
         self.needToUpdateSpec=True
     
-    def get_heterodyning_spectrogram(self,win_time,overlap_time):
+    def _get_heterodyning_spectrogram(self,data_dict,win_time,overlap_time):
         Chan='Channel_1'
-        dt=self.dict[Chan]['x_increment']
-        freq, time, spec=scipy.signal.spectrogram(self.dict[Chan]['data']-np.mean(self.dict[Chan]['data']),
+        dt=data_dict[Chan]['x_increment']
+        freq, time, spec=scipy.signal.spectrogram(data_dict[Chan]['data']-np.mean(data_dict[Chan]['data']),
                                                   1/dt,
                                                   window='triang',
                                                   nperseg=int(win_time/dt),
@@ -65,10 +85,10 @@ class Data():
         spec=np.rot90(spec)
         return freq,time,spec
     
-    def get_power_spectrogram(self,win_time,overlap_time):
+    def _get_power_spectrogram(self,data_dict,win_time,overlap_time):
         Chan='Channel_3'
-        dt=self.dict[Chan]['x_increment']
-        freq, time, spec=scipy.signal.spectrogram(self.dict[Chan]['data']-np.mean(self.dict[Chan]['data']),
+        dt=data_dict[Chan]['x_increment']
+        freq, time, spec=scipy.signal.spectrogram(data_dict[Chan]['data']-np.mean(data_dict[Chan]['data']),
                                                   1/dt,
                                                   window='triang',
                                                   nperseg=int(win_time/dt),
@@ -79,18 +99,19 @@ class Data():
         spec=np.rot90(spec)
         return freq,time,spec
     
-    def process_spectrogram(self):
-        freq, time, spec_1=self.get_heterodyning_spectrogram(self.win_time,self.overlap_time)
+    def process_spectrogram(self,f_name):
+        data_dict=b_reader.read(f_name)
+        freq, time, spec_1=self._get_heterodyning_spectrogram(data_dict,self.win_time,self.overlap_time)
         m1=np.mean(spec_1)
         s1=np.std(spec_1)
-        freq, time, spec_2=self.get_power_spectrogram(self.win_time,self.overlap_time)
+        freq, time, spec_2=self._get_power_spectrogram(data_dict,self.win_time,self.overlap_time)
         m3=np.mean(spec_2)
         s3=np.std(spec_2)
         mask=spec_2>m3+2*s3
         spec_1[mask]=m1-s1*np.random.ranf(mask[mask==True].size)
 
     
-        average_factor_for_freq=int(self.average_freq_window/(1/2/self.dt/len(freq)))
+        average_factor_for_freq=int(self.average_freq_window/(freq[1]-freq[0]))
         average_factor_for_times=int(self.average_time_window/(self.win_time-self.overlap_time))
         
         if self.IsAveraging:
@@ -121,15 +142,18 @@ class Data():
         self.ax_spec=ax
         
     def find_modes(self,indicate_modes_on_spectrogram=False):
+        self.modes=[]
         signal_shrinked=np.nanmax(self.processed_spec,axis=0)
         mode_indexes,_=scipy.signal.find_peaks(signal_shrinked, height=bn.nanstd(signal_shrinked),prominence=bn.nanstd(signal_shrinked))#distance=self.average_freq_window/(1/2/self.dt/len(self.freq)))
-        if self.fig_spec is not None and indicate_modes_on_spectrogram:
-            for p in mode_indexes:
+        for p in mode_indexes:
+            self.modes.append(Mode(p,self.freq[p]))
+            if self.fig_spec is not None and indicate_modes_on_spectrogram:
                 plt.axvline(self.freq[p],color='white')
-        self.mode_indexes=mode_indexes
-        return mode_indexes 
+            
+        return self.modes
     
-    def plot_mode_dynamics(self,mode_index):
+    def plot_mode_dynamics(self,mode_number):
+        mode_index=self.modes[mode_number].ind
         signal=self.processed_spec[:,mode_index]
         fig=plt.figure()
         plt.plot(self.time,signal)
@@ -137,9 +161,9 @@ class Data():
         plt.gca().yaxis.set_major_formatter(formatter1)
         plt.xlabel('Time, s')      
         plt.ylabel('Intensity, arb.u.')
-        plt.title('Mode at {:.3f} MHz'.format(self.freq[mode_index]/1e6))
+        plt.title('Mode at {:.3f} MHz detuning'.format(self.freq[mode_index]/1e6))
         plt.tight_layout()
-        return fig
+        return fig, plt.gca()
         
     def plot_mode_lowfreq_spectrum(self,mode_index):
         signal=self.processed_spec[:,mode_index]
@@ -156,25 +180,39 @@ class Data():
         plt.gca().set_ylim((0,5e-6))
         plt.gca().xaxis.set_major_formatter(formatter1)
         plt.gca().yaxis.set_major_formatter(formatter1)
-        plt.title('Mode spectrum at {:.3f} MHz'.format(self.freq[mode_index]/1e6))
+        plt.title('RF spectrum for mode at {:.3f} MHz detuning'.format(self.freq[mode_index]/1e6))
         plt.tight_layout()
-        return fig
+        return fig, plt.gca()
 
-    def get_mode_lifetime(self,mode_index):
+    def get_mode_lifetime(self,mode_number):
+        mode_index=self.modes[mode_number].ind
         signal=self.processed_spec[:,mode_index]
         average_factor_for_times=int(self.average_time_window/(self.win_time-self.overlap_time))
-        peaks,_=scipy.signal.find_peaks(signal, height=5*bn.nanstd(signal),width=average_factor_for_times,prominence=3*np.nanstd(signal))
-        widths,width_heights,left_ips, right_ips=scipy.signal.peak_widths(signal,peaks,rel_height=0.9)
-        return self.time[int(right_ips[0])]-self.time[int(left_ips[0])]
+        signal=bn.move_mean(signal,100)
+        peak=np.nanargmax(signal)
+        # peaks,_=scipy.signal.find_peaks(signal, height=3*bn.nanstd(signal),width=average_factor_for_times,prominence=3*np.nanstd(signal))
+        widths,width_heights,left_ips, right_ips=scipy.signal.peak_widths(signal,[peak],rel_height=0.9)
+        print(self.time[int(left_ips[0])])
+        print(self.time[int(right_ips[0])])
+        self.modes[mode_number].birth_time=self.time[int(left_ips[0])]
+        self.modes[mode_number].death_time=self.time[int(right_ips[0])]
+        self.modes[mode_number].life_time=self.modes[mode_number].death_time-self.modes[mode_number].birth_time
+        
+        # plt.hlines(0,xmin=self.modes[mode_number].birth_time,xmax=self.modes[mode_number].death_time,color='red')
+        return self.modes[mode_number].life_time,self.modes[mode_number].birth_time,self.modes[mode_number].death_time
     
     def get_lifetime_hist(self):
         life_times=[]
-        if self.mode_indexes is not None:
-            for p in self.mode_indexes:
+        if self.modes is not None:
+            for p,_ in enumerate(self.modes['ind']):
                 life_times.append(self.get_mode_lifetime(p))
+            plt.hist(life_times)
+            plt.xlabel('Time, s')
+            plt.ylabel('Number of modes')       
+            plt.gca().xaxis.set_major_formatter(formatter1)
             
 
         
 if __name__=='__main__':
-    print('')
+    print()
     
