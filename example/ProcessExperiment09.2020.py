cc15f66c773381d834712e743bfc9625360d5a08
IlyaVatnik <ilya.vatnik@gmail.com>
2020-10-02 20:40:33 +0700

    Update ProcessExperiment09.2020.py

--- example/ProcessExperiment09.2020.py
+++ example/ProcessExperiment09.2020.py
@@ -9,8 +9,8 @@ import bottleneck as bn
 from heterodyning.processing import Spectrogram
 
 
-folder='Data\\'
-file_name='2gss-29.9dBm-0.bin'
+folder='G:\!Projects\!Rayleigh lasers - localisation, heterodyne, coherent detection\heterodyning\example\\'
+file_name='pulse 10mks edge 3 ns 4 V.bin'
 formatter1 = EngFormatter(places=2)  # U+2009
 
 
@@ -29,15 +29,15 @@ time0=time.time()
 spec=Spectrogram(folder+file_name,win_time,overlap_time,IsAveraging,average_time_window,average_freq_window)
 spec.plot_processed_spectrogram()
 
-spec.find_modes(indicate_modes_on_spectrogram=False)
+spec.find_modes(indicate_modes_on_spectrogram=True)
 
 
 
 for p,_ in enumerate(spec.modes):
     fig,ax=spec.plot_mode_dynamics(p)
-    life_time,l,r=spec.get_mode_lifetime(p)
-    ax.set_xlim((l-2e-3,r+2e-3))
-    spec.plot_mode_lowfreq_spectrum(p)
+#     life_time,l,r=spec.get_mode_lifetime(p)
+#     ax.set_xlim((l-2e-3,r+2e-3))
+#     spec.plot_mode_lowfreq_spectrum(p)
 
 
 time1=time.time()
